faa6c965387849f8d2d6ccb72e6dd1ef9ce86382
D-Pow <djp460@nyu.edu>
2018-11-11 13:33:58 -0500

    Add script for creating components
    Using 'npm run create MyComponent' will automatically create the component folder src/components/MyComponent/ with index.js and MyComponent.js

---
+++ createComponent.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+const COMPONENT_NAME_INDEX = 2;
+const DIR_NAME_INDEX = 3;
+
+function processArgs(args) {
+    switch (args.length) {
+        case 3:
+            createComponent(args[COMPONENT_NAME_INDEX]);
+            break;
+        case 4:
+            createClass(args[COMPONENT_NAME_INDEX], args[DIR_NAME_INDEX]);
+            break;
+        default:
+            printUsage();
+    }
+}
+
+function createComponent(componentName = "MyComponent") {
+    createClass(componentName, 'components');
+}
+
+function createClass(componentName, dirName = 'components') {
+    const dir = `./src/${dirName}/${componentName}`;
+    const indexText = `import ${componentName} from './${componentName}';\n\nexport default ${componentName};\n`;
+    fs.mkdir(`${dir}`, {recursive: true}, err => {
+        if (err) {
+            error(err);
+        } else {
+            fs.writeFile(`${dir}/index.js`, indexText, err => {
+                if (err) {
+                    error(err);
+                } else {
+                    fs.writeFile(`${dir}/${componentName}.js`, '', err => {
+                        if (err) {
+                            error(err);
+                        } else {
+                            console.log(`Created ${componentName} in src/${dirName}/`);
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
+
+function printUsage() {
+    const usage = "Creates a component inside its own folder in the src/ directory along with an index.js file" +
+        "\nUsage: createComponent NAME [directory under src/]";
+    console.log(usage);
+    process.exit(0);
+}
+
+function error(err) {
+    console.error(err);
+    process.exit(1);
+}
+
+processArgs(process.argv);
