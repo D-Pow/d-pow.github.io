5c6cddab0c4df0d406dd821a1d44e6710309d32c
D-Pow <djp460@nyu.edu>
2018-12-27 15:21:01 -0500

    Move to iterative Triangle creation

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,12 +3,26 @@ import 'styles/Header.scss';
 import Triangle from 'components/Triangle';
 
 class Home extends React.Component {
+    renderTriangles() {
+        const row = [];
+        for (let i = 0; i <= Triangle.CONFIG.numTrianglesInRow; i++) {
+            const color = Triangle.CONFIG.randomColor();
+            row.push((
+                <Triangle color={color} key={i} spinDelay={-5} upsideDown={i % 2 === 0} />
+            ));
+        }
+        return (
+            <div className={'flex-row'} style={{height: '1000px'}}>
+                {row}
+            </div>
+        );
+    }
+
     render() {
         return (
-            <div style={{height: '1000px'}}>
-                <Triangle color={'purple'} spinDelay={5} />
-                <Triangle color={'blue'} upsideDown={true} />
-            </div>
+            <React.Fragment>
+                {this.renderTriangles()}
+            </React.Fragment>
         );
     }
 }
