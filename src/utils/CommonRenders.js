63faf01eebedd5012054c427e8a3ae0822e5ec06
D-Pow <djp460@nyu.edu>
2019-07-22 22:21:11 -0400

    Add file for common rendered components

---
+++ src/utils/CommonRenders.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const ETRADE_LINK = (
+    <a href={'https://us.etrade.com/what-we-offer/investment-choices/mutual-funds'} target={'_blank'}>
+        Mutual Fund/ETF
+    </a>
+);
