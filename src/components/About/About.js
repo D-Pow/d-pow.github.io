a20ff59ce78d969b687921d4ff23c7f2777b93f0
D-Pow <djp460@nyu.edu>
2019-07-22 22:27:32 -0400

    Add TODO in About page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 class About extends React.Component {
     render() {
+        // TODO Productions, Projects, Pastimes, Personal Contact
         return (
             <div>About</div>
         );
