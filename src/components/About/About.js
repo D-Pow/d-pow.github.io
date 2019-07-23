bfaa299a77b0f98e25bcf367270ad5b4b56d2514
D-Pow <djp460@nyu.edu>
2019-07-22 22:27:17 -0400

    Move About.js to its own folder

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 class About extends React.Component {
     render() {
         return (
-            <React.Fragment>About</React.Fragment>
+            <div>About</div>
         );
     }
 }
