8d3b7052ccb3a98fcad3f3e3137f79279ccba77a
D-Pow <djp460@nyu.edu>
2019-07-22 22:21:43 -0400

    Add starter InfoCard content in GistSection

--- src/components/Home/GistSection/GistSection.js
+++ src/components/Home/GistSection/GistSection.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ScrollToShow from 'components/ui/ScrollToShow';
+import InfoCard from 'components/ui/InfoCard';
+import { ETRADE_LINK } from 'utils/CommonRenders';
 
 class GistSection extends React.Component {
+    pageText = {
+        infoCards: [
+            {
+                title: 'Full-stack software engineer',
+                description: (<React.Fragment>I build platforms at E-Trade for creating {ETRADE_LINK} investment plans</React.Fragment>),
+                children: (
+                    <React.Fragment>
+                        <h5>Major interests include:</h5>
+                        <ul>
+                            <li>Design patterns</li>
+                            <li>App architecture</li>
+                            <li>Security</li>
+                            <li>Good source control practices</li>
+                        </ul>
+                    </React.Fragment>
+                )
+            }
+        ]
+    };
+
     render() {
         return (
             <ScrollToShow className={'margin-center container'} addClasses={'fade-in-show'} distributeClasses={'fade-in'}>
                 <h2 className={'p-5'}>The gist...</h2>
-                <h4>Full-stack developer</h4>
+                {this.pageText.infoCards.map((content, index) => (<InfoCard {...content} key={index} />))}
             </ScrollToShow>
         );
     }
