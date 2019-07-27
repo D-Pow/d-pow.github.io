983759f9488c6fe17fb1d6a923f3627841f35ace
D-Pow <djp460@nyu.edu>
2019-07-26 23:58:44 -0400

    Improve SCSS map parsing
    Always parse value as a string and make the key a string if it isn't already

--- src/utils/Functions.js
+++ src/utils/Functions.js
@@ -1,8 +1,21 @@
 import { COLORS, MOBILE_BROWSER_REGEX } from './Constants';
 import { themeColors } from 'styles/Common.scss';
 
+/**
+ * Parses a map into a JS object. Every value for any key will be a string
+ *
+ * @param scssMapStr {string} - String imported from a .scss file
+ * @returns {Object} - The parsed SCSS object
+ */
 export function parseScssMap(scssMapStr) {
-    return JSON.parse(scssMapStr.replace('(', '{').replace(')', '"}').replace(/([#,])/g, '"$1'));
+    return JSON.parse(scssMapStr
+        .replace('(', '{')
+        .replace(')', '}')
+        // JSON values: colon + space, bunch of word characters followed by a comma or bracket
+        .replace(/: ([^,}]+)([,}])/g, ': "$1"$2')
+        // JSON keys: space or bracket as first character, not already a string, anything not colon or space (rules out JSON values), ended by colon
+        .replace(/([\s{])(?!")([^:\s]+)+:/g, '$1"$2":')
+    );
 }
 
 export function randomColor(colorsToAvoid, onlyColors = null) {
