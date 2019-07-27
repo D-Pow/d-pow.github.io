ce692bd96403629efc0e2da0cdf46f5c8778bc43
D-Pow <djp460@nyu.edu>
2019-07-27 00:02:32 -0400

    Add mobile InfoCard rendering
    Use bootstrap grid breakpoints in the logic to determine where the forwarded ref should go

--- src/components/ui/InfoCard/InfoCard.js
+++ src/components/ui/InfoCard/InfoCard.js
@@ -1,7 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { parseScssMap } from 'utils/Functions';
+import { gridBreakpoints } from 'styles/Common.scss';
 
 class InfoCard extends React.Component {
+    renderDesktop(pageContent) {
+        return (
+            <div className={`d-none d-sm-block ${this.props.className}`}>
+                <div className={'row'} ref={this.props.forwardedRef}>
+                    <div className={'col-sm-6 margin-center'}>
+                        {pageContent[Number(this.props.flipped)]}
+                    </div>
+                    <div className={'col-sm-6'}>
+                        {pageContent[Number(!this.props.flipped)]}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    renderMobile(textContent) {
+        return (
+            <div className={`d-block d-sm-none ${this.props.className}`} ref={this.props.forwardedRef}>
+                <div className={'row margin-center mb-4'}>
+                    {textContent}
+                </div>
+                <div>
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+
     render() {
         const textContent = (
             <React.Fragment>
@@ -11,16 +41,11 @@ class InfoCard extends React.Component {
         );
         const pageContent = [ textContent, this.props.children ];
 
-        return (
-            <div className={`row ${this.props.className}`} ref={this.props.forwardedRef}>
-                <div className={'col-sm-6 margin-center'}>
-                    {pageContent[Number(this.props.flipped)]}
-                </div>
-                <div className={'col-sm-6'}>
-                    {pageContent[Number(!this.props.flipped)]}
-                </div>
-            </div>
-        );
+        // Since refs only work when attached to the actual div they pertain to, render only one of either desktop or mobile
+        const mobileThreshold = Number(parseScssMap(gridBreakpoints).sm.replace(/\D/g, ''));
+        const isMobile = window.innerWidth < mobileThreshold;
+
+        return isMobile ? this.renderMobile(textContent) : this.renderDesktop(pageContent);
     }
 }
 
