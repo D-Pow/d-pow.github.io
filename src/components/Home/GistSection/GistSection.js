63fa936c6e9bb8c95d2c50b6c4e75ba896a10de1
D-Pow <djp460@nyu.edu>
2019-08-02 22:00:42 -0400

    Move fade-in animation to Animations/ and remove duplicate code

--- src/components/Home/GistSection/GistSection.js
+++ src/components/Home/GistSection/GistSection.js
@@ -23,7 +23,7 @@ class GistSection extends React.Component {
     renderGistSection() {
         return (
             <React.Fragment>
-                <ScrollToShow addClasses={'fade-in-show'} distributeClasses={'fade-in'}>
+                <ScrollToShow addClasses={'show'} distributeClasses={'fade-in'}>
                     <h1 className={'p-5 mb-5 bg-light'}>The gist...</h1>
                 </ScrollToShow>
                 <div className={'container'}>
