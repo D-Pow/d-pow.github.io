b09a4c79f31cbf912d143ccd42342e00e8fc4b41
D-Pow <djp460@nyu.edu>
2019-08-02 21:43:47 -0400

    Update gist section with better description and profile pic

--- src/components/Home/GistSection/GistSection.js
+++ src/components/Home/GistSection/GistSection.js
@@ -1,37 +1,56 @@
 import React from 'react';
 import ScrollToShow from 'components/ui/ScrollToShow';
 import InfoCard from 'components/ui/InfoCard';
+import Image from 'components/ui/Image';
 import { ETRADE_LINK, ETRADE_MUTUAL_FUNDS_LINK } from 'utils/CommonRenders';
 
 class GistSection extends React.Component {
     pageText = {
-        infoCards: [
-            {
-                title: 'Full-stack software engineer',
-                description: (<React.Fragment>I build platforms at {ETRADE_LINK} for creating {ETRADE_MUTUAL_FUNDS_LINK} investment plans</React.Fragment>),
-                children: (
-                    <React.Fragment>
-                        <h5>Major interests include:</h5>
-                        <ul>
-                            <li>Design patterns</li>
-                            <li>App architecture</li>
-                            <li>Security</li>
-                            <li>Good source control practices</li>
-                        </ul>
-                    </React.Fragment>
-                )
-            }
-        ]
+        gist: {
+            title: 'Full-stack software engineer',
+            description: (
+                <React.Fragment>
+                    I build platforms at {ETRADE_LINK} for creating {ETRADE_MUTUAL_FUNDS_LINK} investment plans.
+                    I have a passion for good coding practices, both for humans (readability, design patterns) and
+                    computers (efficiency). Enjoying my work is all about learning new things, making meaningful
+                    contributions, and collaborating with my teammates, because it's the people who make the workplace
+                    worthwhile.
+                </React.Fragment>
+            )
+        }
     };
+
+    renderGistSection() {
+        return (
+            <React.Fragment>
+                <ScrollToShow addClasses={'fade-in-show'} distributeClasses={'fade-in'}>
+                    <h1 className={'p-5 mb-5 bg-light'}>The gist...</h1>
+                </ScrollToShow>
+                <div className={'container'}>
+                    <InfoCard
+                        className={'mb-5'}
+                        mainContent={(
+                            <ScrollToShow addClasses={'slide-in-left show'} distributeClasses={' animated'}>
+                                {InfoCard.renderDefaultTextContent(this.pageText.gist.title, this.pageText.gist.description)}
+                            </ScrollToShow>
+                        )}
+                    >
+                        <ScrollToShow addClasses={'slide-in-right show'} distributeClasses={'animated'}>
+                            <div>
+                                <Image image={'profile_pic.jpg'} aria={{style: { width: '80%' }}}/>
+                            </div>
+                        </ScrollToShow>
+                    </InfoCard>
+                </div>
+            </React.Fragment>
+        );
+    }
 
     render() {
         return (
-            <ScrollToShow className={'margin-center container'} addClasses={'fade-in-show'} distributeClasses={'fade-in'}>
-                <h2 className={'p-5'}>The gist...</h2>
-                {this.pageText.infoCards.map((content, index) => (
-                    <InfoCard className={'mb-5'} {...content} flipped={Boolean(index % 2)} key={index} />
-                ))}
-            </ScrollToShow>
+            <React.Fragment>
+                {this.renderGistSection()}
+            </React.Fragment>
         );
     }
 }
