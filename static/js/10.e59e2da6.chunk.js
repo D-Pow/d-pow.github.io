374a33c1ea7c6ba171670b63ee032baf8e50871b
D-Pow <djp460@nyu.edu>
2019-08-10 18:34:55 -0400

    Updates

---
+++ static/js/10.e59e2da6.chunk.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{353:function(o,s,c){o.exports=c.p+"static/assets/favicon-56e699cd.ico"}}]);
---
+++ static/js/12.e59e2da6.chunk.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{355:function(p,s,o){p.exports=o.p+"static/assets/profile_pic-16908fed.jpg"}}]);
