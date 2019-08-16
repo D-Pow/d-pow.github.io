50067af13452b5cb8f443fe773cb068f93d3cdd6
D-Pow <djp460@nyu.edu>
2019-08-16 13:00:17 -0400

    Add object field validation util
    Instead of the classic 'obj && obj.a && obj.a.b && doSomething()' code, use recursive validation function to clean code dramatically

--- src/utils/Functions.js
+++ src/utils/Functions.js
@@ -102,3 +102,32 @@ export async function loadImage(image, base64 = false) {
 
     throw new Error(`Image at ${image} was not found`);
 }
+
+/**
+ * Checks if all fields passed into the function exist nested
+ * inside each other. This does not check if multiple
+ * fields exist in a given level inside the object, only that fields
+ * exist inside other fields.
+ * If a field exists, but is null or undefined, then this will
+ * return false.
+ *
+ * @param {Object} obj - Object to check validity of nested fields, e.g. network response or ref
+ * @param {...string} nestedFields - Spread of nested fields to check in order
+ * @returns {boolean} If the obj contains all given nested fields and they are not null/undefined
+ */
+export function validateObjNestedFields(obj, ...nestedFields) {
+    const fieldsArray = (nestedFields[0] instanceof Array) ? nestedFields[0] : nestedFields;
+    const responseExists = obj != null;
+
+    if (fieldsArray.length === 0) {
+        return responseExists;
+    }
+
+    const nextField = fieldsArray[0];
+
+    return (
+        responseExists
+        && obj.hasOwnProperty(nextField)
+        && validateObjNestedFields(obj[nextField], fieldsArray.slice(1))
+    );
+}
