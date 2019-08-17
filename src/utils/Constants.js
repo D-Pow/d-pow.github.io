5668ea758b6c87c3bd6150b1bbea90795b2067bb
D-Pow <djp460@nyu.edu>
2019-08-17 14:03:54 -0400

    Add URLs for projects/publications

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -29,5 +29,8 @@ export const LINKS = {
     MutualFundsHome: 'https://us.etrade.com/what-we-offer/investment-choices/mutual-funds',
     PrebuiltPortfolios: 'https://us.etrade.com/etx/wm/prebuiltmutualfundportfolios',
     AutomaticInvesting: 'https://us.etrade.com/etx/wm/automaticinvesting',
-    EtradeAndroidApp: 'https://play.google.com/store/apps/details?id=com.etrade.mobilepro.activity'
+    EtradeAndroidApp: 'https://play.google.com/store/apps/details?id=com.etrade.mobilepro.activity',
+    AtomsOfConfusion: 'https://atomsofconfusion.com',
+    AnticancerPeptides: 'https://www.ncbi.nlm.nih.gov/pubmed/29897657',
+    TardigradeStratification: 'https://bioone.org/journals/Transactions-of-the-Kansas-Academy-of-Science/volume-118/issue-3-4/062.118.0306/Tardigrades-of-the-Canopy-Evidence-of-Stratification/10.1660/062.118.0306.short'
 };
