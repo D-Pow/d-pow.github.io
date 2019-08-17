26150c54609e7bea31123734fc165dd63e389c85
D-Pow <djp460@nyu.edu>
2019-08-17 13:25:33 -0400

    Move URL links to Constants

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -24,3 +24,10 @@ export const COLORS = [
 export const SHOW_ELEMENT_SCROLL_THRESHOLD = 3/4;
 
 export const MOBILE_BROWSER_REGEX = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i;
+
+export const LINKS = {
+    MutualFundsHome: 'https://us.etrade.com/what-we-offer/investment-choices/mutual-funds',
+    PrebuiltPortfolios: 'https://us.etrade.com/etx/wm/prebuiltmutualfundportfolios',
+    AutomaticInvesting: 'https://us.etrade.com/etx/wm/automaticinvesting',
+    EtradeAndroidApp: 'https://play.google.com/store/apps/details?id=com.etrade.mobilepro.activity'
+};
