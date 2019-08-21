7aaf3b0bf7c10f8eff411723243fc47eb2d592ee
D-Pow <djp460@nyu.edu>
2019-08-20 20:28:36 -0400

    Make createComponent.js script able to create functional components

--- scripts/createComponent.js
+++ scripts/createComponent.js
@@ -3,31 +3,9 @@ const fs = require('fs');
 const COMPONENT_NAME_INDEX = 2;
 const DIR_NAME_INDEX = 3;
 
-function processArgs(args) {
-    switch (args.length) {
-        case 3:
-            createComponent(args[COMPONENT_NAME_INDEX]);
-            break;
-        case 4:
-            createClass(args[COMPONENT_NAME_INDEX], args[DIR_NAME_INDEX]);
-            break;
-        default:
-            printUsage();
-    }
-}
-
-function createComponent(componentName = "MyComponent") {
-    createClass(componentName, 'components');
-}
-
-function createClass(componentName, dirName = 'components') {
-    const dir = `./src/${dirName}/${componentName}`;
-    const indexText = `import ${componentName} from './${componentName}';\n\nexport default ${componentName};\n`;
-    const componentText = 
-`import React from 'react';
-import PropTypes from 'prop-types';
-
-class ${componentName} extends React.Component {
+function getClassComponentText(componentName) {
+    return (
+`class ${componentName} extends React.Component {
     constructor(props) {
         super(props);
     }
@@ -37,7 +15,26 @@ class ${componentName} extends React.Component {
             <div></div>
         );
     }
+}`
+    );
 }
+
+function getFunctionalComponentText(componentName) {
+    return (
+`function ${componentName}(props) {
+    return (
+        <div></div>
+    );
+}`
+    );
+}
+
+function getComponentText(componentName, functional = false) {
+    return (
+`import React from 'react';
+import PropTypes from 'prop-types';
+
+${functional ? getFunctionalComponentText(componentName) : getClassComponentText(componentName)}
 
 ${componentName}.propTypes = {
 
@@ -48,7 +45,35 @@ ${componentName}.defaultProps = {
 };
 
 export default ${componentName};
-`;
+`
+    );
+}
+
+function processArgs(args) {
+    const functionalComponentFlagIndex = args.indexOf('func');
+    const functionalComponentFlag = functionalComponentFlagIndex >= 0;
+
+    if (functionalComponentFlag) {
+        args.splice(functionalComponentFlagIndex, 1);
+    }
+
+    switch (args.length) {
+        case 3:
+            createClass(args[COMPONENT_NAME_INDEX], 'components', functionalComponentFlag);
+            break;
+        case 4:
+            createClass(args[COMPONENT_NAME_INDEX], args[DIR_NAME_INDEX], functionalComponentFlag);
+            break;
+        default:
+            printUsage();
+    }
+}
+
+function createClass(componentName, dirName = 'components', functionalComponent = false) {
+    const dir = `./src/${dirName}/${componentName}`;
+    const indexText = `import ${componentName} from './${componentName}';\n\nexport default ${componentName};\n`;
+    const componentText = getComponentText(componentName, functionalComponent);
+
     fs.mkdir(`${dir}`, {recursive: true}, err => {
         if (err) {
             error(err);
@@ -72,7 +97,7 @@ export default ${componentName};
 
 function printUsage() {
     const usage = "Creates a component inside its own folder in the src/ directory along with an index.js file" +
-        "\nUsage: createComponent NAME [directory under src/]";
+        "\nUsage: createComponent NAME [directory under src/] [func|make functional component]";
     console.log(usage);
     process.exit(0);
 }
