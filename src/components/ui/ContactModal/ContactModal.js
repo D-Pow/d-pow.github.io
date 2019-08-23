a6f8a3713ab95b86ce4bc37ed8f826f8afe4bc33
D-Pow <djp460@nyu.edu>
2019-08-22 21:03:06 -0400

    Convert ContactModal to class component
    In the next set of commits, there will be validation and form submission. Since the logic will be getting more complex, convert to a class component so that concerns are separated and the functionality is easier to read

--- src/components/ui/ContactModal/ContactModal.js
+++ src/components/ui/ContactModal/ContactModal.js
@@ -1,69 +1,76 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'components/ui/Modal';
 
-function ContactModal({ show, handleClose }) {
-    const [ hasSubmitted, setHasSubmitted ] = useState(false);
-    const [ hasClosedAfterSubmitting, setHasClosedAfterSubmitting ] = useState(false);
-    const [ nameInput, setNameInput ] = useState('');
-    const [ emailInput, setEmailInput ] = useState('');
-    const [ messageInput, setMessageInput ] = useState('');
+class ContactModal extends React.Component {
+    state = {
+        hasSubmitted: false,
+        hasClosedAfterSubmitting: false,
+        nameInput: '',
+        emailInput: '',
+        messageInput: ''
+    };
 
-    const handleTyping = setterFn => {
+    handleTyping = field => {
         return event => {
-            setterFn(event.target.value);
+            this.setState({ [`${field}Input`]: event.target.value });
         }
     };
-    const handleSubmit = () => {
-        setHasSubmitted(true);
+
+    handleSubmit = () => {
+        this.setState({ hasSubmitted: true });
     };
-    const handleCloseModal = () => {
-        if (hasSubmitted) {
-            setHasClosedAfterSubmitting(true);
+
+    handleCloseModal = () => {
+        if (this.state.hasSubmitted) {
+            this.setState({ hasClosedAfterSubmitting: true });
         }
 
-        handleClose();
+        this.props.handleClose();
     };
 
-    let modalBody;
-    if (hasClosedAfterSubmitting) {
-        modalBody = (
-            <h6>Didn't you already contact me?</h6>
-        );
-    } else if (hasSubmitted) {
-        modalBody = (
-            <React.Fragment>
-                <h5>Thank you!</h5>
-                <p>I will be in contact with you shortly</p>
-            </React.Fragment>
-        );
-    } else {
-        modalBody = (
-            <div className={'form-group'}>
-                <input className={'form-control bg-secondary mb-2 text-white'} type={'text'} placeholder={'Name'} value={nameInput} onChange={handleTyping(setNameInput)} />
-                <input className={'form-control bg-secondary mb-2 text-white'} type={'email'} placeholder={'Email'} value={emailInput} onChange={handleTyping(setEmailInput)} />
-                <textarea className={'form-control bg-secondary text-white'} rows={3} placeholder={'What\'s on your mind?'} value={messageInput} onChange={handleTyping(setMessageInput)} />
+    render() {
+        let modalBody;
+
+        if (this.state.hasClosedAfterSubmitting) {
+            modalBody = (
+                <h6>Didn't you already contact me?</h6>
+            );
+        } else if (this.state.hasSubmitted) {
+            modalBody = (
+                <React.Fragment>
+                    <h5>Thank you!</h5>
+                    <p>I will be in contact with you shortly</p>
+                </React.Fragment>
+            );
+        } else {
+            modalBody = (
+                <div className={'form-group'}>
+                    <input className={'form-control bg-secondary mb-2 text-white'} type={'text'} placeholder={'Name'} value={this.state.nameInput} onChange={this.handleTyping('name')} />
+                    <input className={'form-control bg-secondary mb-2 text-white'} type={'email'} placeholder={'Email'} value={this.state.emailInput} onChange={this.handleTyping('email')} />
+                    <textarea className={'form-control bg-secondary text-white'} rows={3} placeholder={'What\'s on your mind?'} value={this.state.messageInput} onChange={this.handleTyping('message')} />
+                </div>
+            );
+        }
+
+        const modalFooter = this.state.hasSubmitted ? '' : (
+            <div className={'text-center'}>
+                <button className={'btn btn-primary'} onClick={this.handleSubmit}>Send</button>
             </div>
         );
+
+        return (
+            <Modal
+                title={'Get in touch'}
+                footer={modalFooter}
+                useGridForFooter={true}
+                show={this.props.show}
+                onClose={this.handleCloseModal}
+            >
+                {modalBody}
+            </Modal>
+        );
     }
-
-    const modalFooter = hasSubmitted ? '' : (
-        <div className={'text-center'}>
-            <button className={'btn btn-primary'} onClick={handleSubmit}>Send</button>
-        </div>
-    );
-
-    return (
-        <Modal
-            title={'Get in touch'}
-            footer={modalFooter}
-            useGridForFooter={true}
-            show={show}
-            onClose={handleCloseModal}
-        >
-            {modalBody}
-        </Modal>
-    );
 }
 
 ContactModal.propTypes = {
