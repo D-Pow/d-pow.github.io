6f9db81810c8f2f78a7650029f567c57794bdb3f
D-Pow <djp460@nyu.edu>
2019-08-22 22:03:45 -0400

    Add email-validation regex

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -25,6 +25,9 @@ export const SHOW_ELEMENT_SCROLL_THRESHOLD = 3/4;
 
 export const MOBILE_BROWSER_REGEX = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i;
 
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/email#Basic_validation
+export const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
 export const LINKS = {
     MutualFundsHome: 'https://us.etrade.com/what-we-offer/investment-choices/mutual-funds',
     PrebuiltPortfolios: 'https://us.etrade.com/etx/wm/prebuiltmutualfundportfolios',
