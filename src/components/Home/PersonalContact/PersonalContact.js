0dfdfe29bee8a12ef502293a2e02fffbb1f81f8d
D-Pow <djp460@nyu.edu>
2019-08-23 11:23:59 -0400

    Add ContactModal in Personal Contact section with basic show/hide functionality

--- src/components/Home/PersonalContact/PersonalContact.js
+++ src/components/Home/PersonalContact/PersonalContact.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ScrollToShow from 'components/ui/ScrollToShow';
+import ContactModal from 'components/ui/ContactModal';
 
 function PersonalContact(props) {
+    const [ showModal, setShowModal ] = useState(false);
+
+    const showContactModal = () => setShowModal(true);
+    const handleCloseModal = () => setShowModal(false);
+
     return (
         <div className={'mb-5'}>
             <ScrollToShow addClasses={'show'} distributeClasses={'fade-in duration-20'}>
                 <h1 className={'p-5 mb-5 bg-light'}>Personal Contact</h1>
             </ScrollToShow>
+
+            <h5 className={'p-3'}>
+                Interested in finding out more?
+            </h5>
+            <button className={'btn btn-primary'} onClick={showContactModal}>Contact me!</button>
+
+            <ContactModal show={showModal} handleClose={handleCloseModal} />
         </div>
     );
 }
