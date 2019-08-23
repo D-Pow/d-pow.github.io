b88f405219dcbf132783c2398fdc9e231a47d0b4
D-Pow <djp460@nyu.edu>
2019-08-23 11:49:50 -0400

    Add animation to show ContactModal button on hover

--- src/components/Home/PersonalContact/PersonalContact.js
+++ src/components/Home/PersonalContact/PersonalContact.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
 import ScrollToShow from 'components/ui/ScrollToShow';
 import ContactModal from 'components/ui/ContactModal';
+import { useHover } from 'utils/Hooks';
 
 function PersonalContact(props) {
     const [ showModal, setShowModal ] = useState(false);
+    const [ hoverRef, isHovered ] = useHover();
 
     const showContactModal = () => setShowModal(true);
     const handleCloseModal = () => setShowModal(false);
+
+    const animationCls = 'animated duration-5';
+    const animationShowCls = 'flip-x show';
+    const headerCls = `border border-primary border-medium rounded margin-center p-4 width-fit ${animationCls} ${isHovered ? '' : animationShowCls}`;
+    const btnCls = `btn btn-primary ${animationCls} ${isHovered ? animationShowCls : ''}`;
 
     return (
         <div className={'mb-5'}>
@@ -14,10 +21,20 @@ function PersonalContact(props) {
                 <h1 className={'p-5 mb-5 bg-light'}>Personal Contact</h1>
             </ScrollToShow>
 
-            <h5 className={'p-3'}>
-                Interested in finding out more?
-            </h5>
-            <button className={'btn btn-primary'} onClick={showContactModal}>Contact me!</button>
+            <div className={'container'}>
+                <div className={'row justify-content-sm-center'}>
+                    <div className={'col-sm-6 my-5'}>
+                        <div className={'absolute-center'}>
+                            <h5 className={headerCls} ref={hoverRef}>
+                                Interested in finding out more?
+                            </h5>
+                        </div>
+                        <div className={'absolute-center'}>
+                            <button className={btnCls} onClick={showContactModal}>Contact me!</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <ContactModal show={showModal} handleClose={handleCloseModal} />
         </div>
