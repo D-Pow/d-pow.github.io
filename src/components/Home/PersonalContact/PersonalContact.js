7df24a94c54afb74de27c014191b74dd4c49aa4c
D-Pow <djp460@nyu.edu>
2019-08-23 11:22:58 -0400

    Make 'Personal Contact' title match styling of rest of home page's sections

--- src/components/Home/PersonalContact/PersonalContact.js
+++ src/components/Home/PersonalContact/PersonalContact.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import ScrollToShow from 'components/ui/ScrollToShow';
 
 function PersonalContact(props) {
     return (
-        <h1 className={'p-5 mb-5'}>Personal Contact</h1>
+        <div className={'mb-5'}>
+            <ScrollToShow addClasses={'show'} distributeClasses={'fade-in duration-20'}>
+                <h1 className={'p-5 mb-5 bg-light'}>Personal Contact</h1>
+            </ScrollToShow>
+        </div>
     );
 }
 
