306dfbc81059dcfaa78840ee9fbf464e4524c6ae
D-Pow <djp460@nyu.edu>
2019-11-10 16:01:11 -0500

    Convert ImageCard to functional component
    This cleans up the component noticeably. Also, slight refactoring for easier comprehension: (1) 'ref -> 'hoverRef', (2) remove superfluous '|| defaultWidth' from renderHoverContent() -> width computation

--- src/components/ui/ImageCard/ImageCard.js
+++ src/components/ui/ImageCard/ImageCard.js
@@ -1,86 +1,77 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Image from 'components/ui/Image';
-import { useHover, Hooked } from 'utils/Hooks';
+import { useHover } from 'utils/Hooks';
 import ContextFactory from 'utils/Context';
 import { isMobileBrowser, validateObjNestedFields } from 'utils/Functions';
 
-const defaultContextImgHeight = 'auto';
-const ImageCardSizeContext = ContextFactory(defaultContextImgHeight);
+function ImageCard({ className, image, imageCls, title, description, smallTextMargins, aria }) {
+    const parentDivRef = React.createRef();
+    const imageRef = React.createRef();
+    const [ hoverRef, isHovered ] = useHover();
 
-class ImageCard extends React.Component {
-    static SameHeightConsumer = ImageCardSizeContext.Consumer;
-    static SameHeightProvider = ImageCardSizeContext.Provider;
-
-    parentDivRef = React.createRef();
-    imageRef = React.createRef();
-
-    updateContextHeight = (contextImgHeight, setContextImgHeight) => {
-        const { height } = this.parentDivRef.current.getBoundingClientRect();
+    function updateContextHeight(contextImgHeight, setContextImgHeight) {
+        const { height } = parentDivRef.current.getBoundingClientRect();
 
         if (contextImgHeight === defaultContextImgHeight || contextImgHeight > height) {
             setContextImgHeight(height);
         }
-    };
+    }
 
-    renderHoverContent() {
-        const { title, description, smallTextMargins } = this.props;
+    function renderHoverContent() {
         const positionCls = 'position-absolute fixed-top h-100';
         const animationCls = 'duration-5 linear';
         // Mobile browsers between phone and tablet look strange when using the media query for xs/sm
         // so force all mobile browsers to use the mobile view
         const marginCls = isMobileBrowser() || smallTextMargins ? ['mt-10p mb-5p ml-1', 'mx-5p'] : ['m-5', 'm-3'];
         const Title = isMobileBrowser() ? 'h4' : 'h3';
+        const hoverCls = isHovered ? ['show', 'slide-in-top', 'slide-in-bottom'] : ['', '', ''];
+        const defaultWidth = 'auto';
+        const width = validateObjNestedFields(imageRef, 'current')
+            ? imageRef.current.getBoundingClientRect().width
+            : defaultWidth;
 
         return (
-            <Hooked hook={useHover}>
-                {([ ref, isHovered ]) => {
-                    const hoverCls = isHovered ? ['show', 'slide-in-top', 'slide-in-bottom'] : ['', '', ''];
-                    const defaultWidth = 'auto';
-                    const width = validateObjNestedFields(this.imageRef, 'current')
-                        ? this.imageRef.current.getBoundingClientRect().width || defaultWidth
-                        : defaultWidth;
-
-                    return (
-                        <div
-                            className={`bg-primary margin-center animated fade-in text-light ${positionCls} ${animationCls} ${hoverCls[0]}`}
-                            ref={ref}
-                            style={{ width }}
-                        >
-                            <Title className={`${marginCls[0]} ${animationCls} ${hoverCls[1]}`}>
-                                {title}
-                            </Title>
-                            <p className={`${marginCls[1]} ${animationCls} ${hoverCls[2]}`}>
-                                {description}
-                            </p>
-                        </div>
-                    );
-                }}
-            </Hooked>
+            <div
+                className={`bg-primary margin-center animated fade-in text-light ${positionCls} ${animationCls} ${hoverCls[0]}`}
+                ref={hoverRef}
+                style={{ width }}
+            >
+                <Title className={`${marginCls[0]} ${animationCls} ${hoverCls[1]}`}>
+                    {title}
+                </Title>
+                <p className={`${marginCls[1]} ${animationCls} ${hoverCls[2]}`}>
+                    {description}
+                </p>
+            </div>
         );
     }
 
-    render() {
-        return (
-            <ImageCard.SameHeightConsumer>
-                {({ value: contextImgHeight, setValue: setContextImgHeight }) => (
-                    <div className={this.props.className} ref={this.parentDivRef} {...this.props.aria}>
-                        {/* Obey parent's padding with `position: relative` */}
-                        <div className={'position-relative w-100'} style={{ height: `${contextImgHeight}px`, overflow: 'hidden' }}>
-                            <Image
-                                className={this.props.imageCls}
-                                image={this.props.image}
-                                onLoad={() => this.updateContextHeight(contextImgHeight, setContextImgHeight)}
-                                aria={{ ref: this.imageRef }}
-                            />
-                            {this.renderHoverContent()}
-                        </div>
+    return (
+        <ImageCard.SameHeightConsumer>
+            {({ value: contextImgHeight, setValue: setContextImgHeight }) => (
+                <div className={className} ref={parentDivRef} {...aria}>
+                    {/* Obey parent's padding with `position: relative` */}
+                    <div className={'position-relative w-100'} style={{ height: `${contextImgHeight}px`, overflow: 'hidden' }}>
+                        <Image
+                            className={imageCls}
+                            image={image}
+                            onLoad={() => updateContextHeight(contextImgHeight, setContextImgHeight)}
+                            aria={{ ref: imageRef }}
+                        />
+                        {renderHoverContent()}
                     </div>
-                )}
-            </ImageCard.SameHeightConsumer>
-        );
-    }
+                </div>
+            )}
+        </ImageCard.SameHeightConsumer>
+    );
 }
+
+const defaultContextImgHeight = 'auto';
+const ImageCardSizeContext = ContextFactory(defaultContextImgHeight);
+
+ImageCard.SameHeightConsumer = ImageCardSizeContext.Consumer;
+ImageCard.SameHeightProvider = ImageCardSizeContext.Provider;
 
 ImageCard.propTypes = {
     className: PropTypes.string,
