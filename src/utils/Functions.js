0352c22d6c82b0edaf3bef0cac3905dd2c9f8335
D-Pow <djp460@nyu.edu>
2019-11-16 12:08:10 -0500

    Add JSDoc for getChildName() and childIsOfType() for clarity of usage
    Also, move getChildName() declaration before childIsOfType() since childIsOfType() now calls getChildName()

--- src/utils/Functions.js
+++ src/utils/Functions.js
@@ -5,12 +5,26 @@ export function isMobileBrowser() {
     return MOBILE_BROWSER_REGEX.test(navigator.userAgent || navigator.vendor || window.opera);
 }
 
-export function childIsOfType(child, component) {
-    return (child.type === component) || (getChildName(child) === component);
+/**
+ * Gets the string representation of a single React node
+
+ * @param {Node} child - Child whose name will be retrieved
+ * @returns {string} - The string representation of the node
+ */
+export function getChildName(child) {
+    // child.type.name for React.Component
+    // child.type for HTML elements
+    return child.type.name || child.type;
 }
 
-export function getChildName(child) {
-    return child.type.name || child.type;
+/**
+ *
+ * @param {Node} child - Child whose type to check
+ * @param {(Node|string)} component - What to compare the child to (React.Component or string representation of name)
+ * @returns {boolean} - If the type of the child matches that of the the specified component
+ */
+export function childIsOfType(child, component) {
+    return (child.type === component) || (getChildName(child) === component);
 }
 
 export function childIsReactElement(child) {
