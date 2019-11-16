51b2d4adbecbe6f9f22c9c45f00d565173d9bd68
D-Pow <djp460@nyu.edu>
2019-11-16 11:29:33 -0500

    Move static classNames to div.className
    Since the className values aren't changing, there's no benefit to declaring them outside the div

--- src/components/Home/Productions/Productions.js
+++ src/components/Home/Productions/Productions.js
@@ -45,13 +45,12 @@ function Productions(props) {
                     <SameHeightImageCard.Provider>
                         <ScrollToShow addClasses={'flip-y show'} distributeClasses={'animated duration-15'} distributeSimultaneously={0.5}>
                             {pageText.imageCards.map((imageCardProps, index) => {
-                                const cls = 'col-sm-6 p-0';
                                 // Center if odd number of items and last item
                                 const centerCls = index % 2 === 0 && index === pageText.imageCards.length - 1 ? 'mx-auto' : '';
 
                                 // Nest in div.col so ImageCard's ScrollToShow animation pertains only to image and not containing div
                                 return (
-                                    <div className={`${cls} ${centerCls}`} key={index}>
+                                    <div className={`col-sm-6 p-0 ${centerCls}`} key={index}>
                                         {index !== 0 && (
                                             <div className={'d-block d-sm-none py-2'}>
                                                 <div className={'segment-bar bg-dark'} />
