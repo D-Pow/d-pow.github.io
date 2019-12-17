565f32cc629533b3ad0d4e88019e232bd4263de9
D-Pow <djp460@nyu.edu>
2019-12-17 16:56:21 -0500

    Improve ContextFactory() documentation

--- src/utils/Context.js
+++ src/utils/Context.js
@@ -2,19 +2,48 @@ import React from 'react';
 import { UseContext } from 'utils/Hooks';
 
 /**
- * Creates a new Context and returns both the related Consumer and Provider for component use.
- * Creates a new Context each time this function is called so repeated calls don't have to worry
- * about stepping on each other's toes.
+ * Creates a new Context and returns both the Consumer, Provider, and Context for component use.
+ * Creates a new Context each time this function is called so it may be helpful to call outside
+ * your component declaration depending on your use case.
  *
- * The context created uses both a `value` field and `setValue` function so the consuming components
- * can update the context's state.
+ * The context created passes both a `contextState` field and `setContextState` function to the
+ * created `Provider.props.value` so that consuming components can update the context's state.
  *
- * Consumer takes a function as children:
- * <Consumer>
- *     {({ value, setValue }) => (
- *         {children}
- *     )}
- * </Consumer
+ * Usage:
+ * 1) First, create the context with `ContextFactory(defaultStateValueToStart)`:
+ *    const { Consumer, Provider, Context } = ContextFactory({ firstColor: 'blue', secondColor: 'red'});
+ *
+ * 2) Nest child components inside the Provider; use either Consumer or Context depending on
+ *    if using class or functional components:
+ *    1) Class components: use the returned `Consumer` component, which takes a function as children:
+ *        <Provider>
+ *            <Consumer>
+ *                {({ contextState, setContextState }) => (
+ *                    <MyChild color={contextState.firstColor} />
+ *                    <MyChild color={contextState.secondColor} />
+ *                    <button onClick={() => setContextState({ firstColor: 'red', secondColor: 'blue'})}>
+ *                        Click to change context!
+ *                    </button>
+ *                )}
+ *            </Consumer>
+ *        </Provider>
+ *    2) Functional components: use the returned `Context` object inside a `useContext()` call:
+ *        function MyComponent() {
+ *            const { contextState, setContextState } = useContext(Context);
+ *            return (
+ *                <div>
+ *                    <MyChild color={contextState.firstColor} />
+ *                    <MyChild color={contextState.secondColor} />
+ *                    <button onClick={() => setContextState({ firstColor: 'red', secondColor: 'blue'})}>
+ *                        Click to change context!
+ *                    </button>
+ *                </div>
+ *            );
+ *        }
+ *        // Elsewhere, place your component inside the Provider
+ *        <Provider>
+ *            <MyComponent />
+ *        </Provider>
  *
  * @param {*} defaultValue - Default value for the context
  * @returns {{Consumer: React.Component, Provider: React.Component, Context: Object }} - The newly-created Context-related objects
