cfa063dab043b35ccf3b29094070dbcdf5ce1c1d
D-Pow <djp460@nyu.edu>
2019-12-31 11:57:54 -0500

    Improve splash page title rendering on mobile
    Previously, the title in mobile was covering the main content of the SVG background. Thus, move the title up to be 15% from the top on mobile; also, move it up to 20% from 30% on desktop since it looks slightly better on larger screens

--- src/components/Home/SplashSection/SplashSection.js
+++ src/components/Home/SplashSection/SplashSection.js
@@ -6,16 +6,35 @@ function SplashSection() {
     const pageText = {
         welcomeTitle: "Hey there, I'm Devon!"
     };
-    const renderedWelcomeText = (
-        <div className={'text-center position-absolute w-100 top-30'}>
-            <div className={'bg-dark-fadeout rounded m-auto py-2 px-3 display-3'} style={{ width: '80%' }}>
-                <h1 className={'text-white display-4'}>
-                    <strong>
-                        {pageText.welcomeTitle}
-                    </strong>
-                </h1>
+    const renderedTextOnShadow = (
+        <div className={'container'}>
+            <div className={'row justify-content-center'}>
+                <div className={'col-sm-10'}>
+                    <div className={'bg-dark-fadeout rounded m-auto py-2 px-3 display-3'} style={{ width: '80%' }}>
+                        <h1 className={'text-white display-4'}>
+                            <strong>
+                                {pageText.welcomeTitle}
+                            </strong>
+                        </h1>
+                    </div>
+                </div>
             </div>
         </div>
+    );
+    const absoluteSpacedContainerClasses = 'text-center position-absolute w-100';
+    const renderedAbsoluteSpacedContainer = (
+        <React.Fragment>
+            <div className={'d-block d-sm-none'}>
+                <div className={`${absoluteSpacedContainerClasses} top-15`}>
+                    {renderedTextOnShadow}
+                </div>
+            </div>
+            <div className={'d-none d-sm-block'}>
+                <div className={`${absoluteSpacedContainerClasses} top-20`}>
+                    {renderedTextOnShadow}
+                </div>
+            </div>
+        </React.Fragment>
     );
 
     return (
@@ -25,7 +44,7 @@ function SplashSection() {
                 image={'blue_horizon.svg'}
                 fluidImage={false}
             />
-            {renderedWelcomeText}
+            {renderedAbsoluteSpacedContainer}
         </div>
     );
 }
