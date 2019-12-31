2e8d9355a4f40aac866cb33c10b68a63266e1a37
D-Pow <djp460@nyu.edu>
2019-12-30 20:03:23 -0500

    Display 'personal contact' button after scrolling to show 'Personal Contact' title
    On larger screens, the user cannot scroll down far enough to display the button. Thus, force the button to show once the title shows after 0.5 seconds

--- src/components/Home/PersonalContact/PersonalContact.js
+++ src/components/Home/PersonalContact/PersonalContact.js
@@ -48,11 +48,8 @@ function PersonalContact(props) {
 
     return (
         <div className={'mb-5'}>
-            <ScrollToShow addClasses={'show'} distributeClasses={'fade-in duration-20'}>
+            <ScrollToShow addClasses={'show'} distributeClasses={'fade-in duration-20'} distributeSimultaneously={0.5}>
                 <h1 className={'p-5 mb-5 bg-light'}>Personal Contact</h1>
-            </ScrollToShow>
-
-            <ScrollToShow addClasses={'show'} distributeClasses={'fade-in duration-20'}>
                 <div className={'container'}>
                     <div className={'row justify-content-sm-center'}>
                         <div className={'col-sm-6 my-5'}>
