a122306ac45a89d445407c5920c08d89a5d4a7e4
D-Pow <djp460@nyu.edu>
2019-12-30 19:55:33 -0500

    Convert class component to functional component
    There is no need to maintain the class component structure since there is no longer any complex rendering logic here

--- src/components/Home/SplashSection/SplashSection.js
+++ src/components/Home/SplashSection/SplashSection.js
@@ -2,33 +2,32 @@ import React from 'react';
 import Image from 'components/ui/Image';
 import 'styles/SplashSection.scss';
 
-class SplashSection extends React.Component {
-    pageText = {
+function SplashSection() {
+    const pageText = {
         welcomeTitle: "Hey there, I'm Devon!"
     };
-
-    renderWelcomeText() {
-        return (
-            <div className={'text-center position-absolute w-100 top-30'}>
-                <div className={'bg-dark-fadeout rounded m-auto py-2 px-3 display-3'} style={{width: '80%'}}>
-                    <h1 className={'text-white display-4'}><strong>{this.pageText.welcomeTitle}</strong></h1>
-                </div>
+    const renderedWelcomeText = (
+        <div className={'text-center position-absolute w-100 top-30'}>
+            <div className={'bg-dark-fadeout rounded m-auto py-2 px-3 display-3'} style={{ width: '80%' }}>
+                <h1 className={'text-white display-4'}>
+                    <strong>
+                        {pageText.welcomeTitle}
+                    </strong>
+                </h1>
             </div>
-        );
-    }
+        </div>
+    );
 
-    render() {
-        return (
-            <div className={'overflow-hidden'}>
-                <Image
-                    className={'full-screen'}
-                    image={'blue_horizon.svg'}
-                    fluidImage={false}
-                />
-                {this.renderWelcomeText()}
-            </div>
-        )
-    }
+    return (
+        <div className={'overflow-hidden'}>
+            <Image
+                className={'full-screen'}
+                image={'blue_horizon.svg'}
+                fluidImage={false}
+            />
+            {renderedWelcomeText}
+        </div>
+    );
 }
 
 export default SplashSection;
