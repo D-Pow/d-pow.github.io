48c16c87c890c4e22b64b04481701b888cc0fe36
D-Pow <djp460@nyu.edu>
2020-01-01 16:39:25 -0500

    Reduce SplashSection title's text size on mobile devices
    Makes the title text cover less of the SVG background and allows more words to be on each line (e.g. on smaller screens, 'Hey there,' and 'I'm Devon' fit on two lines instead of spreading them asymmetrically)

--- src/components/Home/SplashSection/SplashSection.js
+++ src/components/Home/SplashSection/SplashSection.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import Image from 'components/ui/Image';
+import { isMobileBrowser } from 'utils/Functions';
 import 'styles/SplashSection.scss';
 
 function SplashSection() {
     const pageText = {
         welcomeTitle: "Hey there, I'm Devon!"
     };
+    const textSize = isMobileBrowser() ? 'display-5' : 'display-4';
     const renderedTextOnShadow = (
         <div className={'container'}>
             <div className={'row justify-content-center'}>
                 <div className={'col-sm-10'}>
                     <div className={'bg-dark-fadeout rounded m-auto py-2 px-3'} style={{ width: '80%' }}>
-                        <h1 className={'text-white display-4'}>
+                        <h1 className={`text-white ${textSize}`}>
                             <strong>
                                 {pageText.welcomeTitle}
                             </strong>
