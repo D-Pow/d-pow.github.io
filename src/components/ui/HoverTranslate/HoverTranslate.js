3ed0edd3664b670578f2525198632380ecdc0730
D-Pow <djp460@nyu.edu>
2020-01-11 18:15:30 -0500

    Add 'aria' prop to HoverTranslate
    For manual attributes passed by parent

--- src/components/ui/HoverTranslate/HoverTranslate.js
+++ src/components/ui/HoverTranslate/HoverTranslate.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useHover } from 'utils/Hooks';
 
-function HoverTranslate({ className, animationCls, english, japanese, passedRef, boundingClientRectForHover }) {
+function HoverTranslate({ className, animationCls, english, japanese, passedRef, boundingClientRectForHover, aria }) {
     const [ ref, isHovered ] = useHover(boundingClientRectForHover);
 
     const renderText = ({ title, description }, show, ref) => (
@@ -15,7 +15,7 @@ function HoverTranslate({ className, animationCls, english, japanese, passedRef,
     // Japanese font is larger than English, so attach ref to Japanese render
     // so that the hover boundary is larger
     return (
-        <div className={className}>
+        <div className={className} {...aria}>
             <div className={'absolute-center'}>
                 {renderText(english, isHovered, passedRef)}
             </div>
@@ -38,14 +38,16 @@ HoverTranslate.propTypes = {
         description: PropTypes.node
     }),
     passedRef: PropTypes.object,
-    boundingClientRectForHover: PropTypes.object
+    boundingClientRectForHover: PropTypes.object,
+    aria: PropTypes.object
 };
 
 HoverTranslate.defaultProps = {
     className: '',
     animationCls: 'animated fade duration-5',
     english: { title: '', description: '' },
-    japanese: { title: '', description: '' }
+    japanese: { title: '', description: '' },
+    aria: {}
 };
 
 export default HoverTranslate;
