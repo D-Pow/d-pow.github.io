d8b047b815c194776fc5088ae7e2d0da92fa80eb
D-Pow <djp460@nyu.edu>
2020-01-12 20:54:59 -0500

    Updates

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,15 +1,51 @@
-// Purpose: Open cache, cache desired files, and confirm success
 var CACHE_NAME = 'cache-v1.0.0';
 var urlsToCache = []; // filenames change in each build (via appended filename hashes), so they can't be predicted here
 
-self.addEventListener('install', event => {
+function removeOldCaches() {
+    return caches.keys()
+        .then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName !== CACHE_NAME;
+                }).map(function(cacheName) {
+                    console.log('Outdated cache', cacheName, 'will be removed');
+                    return caches.delete(cacheName);
+                })
+            );
+        });
+}
+
+function fetchAndCache(event, cache) {
+    return fetch(event.request)
+        .then(function(fetchResponse) {
+            cache.put(event.request, fetchResponse.clone()).catch(function(cacheError) {
+                console.log('Could not cache url:', event.request.url, 'Failed with error:', cacheError);
+            });
+
+            return fetchResponse;
+        })
+        .catch(function(fetchError) {
+            console.log('Could not fetch url:', event.request.url, 'Failed with fetch error:', fetchError);
+        });
+}
+
+self.addEventListener('install', function(event) {
     // Perform install steps
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function(cache) {
                 console.log('Opened cache');
                 return cache.addAll(urlsToCache);
-            }).then(self.skipWaiting()) // needed to force new service workers to overwrite old ones
+            })
+            .then(function() {
+                return self.skipWaiting(); // needed to force new service workers to overwrite old ones
+            })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        removeOldCaches()
     );
 });
 
@@ -18,44 +54,25 @@ self.addEventListener('fetch', event => {
         caches.open(CACHE_NAME).then(function(cache) {
             return cache.match(event.request).then(function(response) {
                 var url = event.request.url;
+                var isIndexHtml = url[url.length-1] === '/' || url.split('/').pop() === 'index.html';
 
-                if (response) {
-                    // Cache hit - return response served from ServiceWorker
+                if (response) { // Cache hit - return response served from ServiceWorker
+                    if (isIndexHtml) {
+                        /**
+                         * If a root level file (like index.html) is requested, then function in a cache-then-network
+                         * fashion. This way, any updates to them will be shown on next page refresh while still
+                         * allowing the old versions of the files to be viewed when offline.
+                         *
+                         * Note: Since all source files have a hash appended to their names, any update in index.html
+                         * will reflect once the page is reloaded. Any change in this service worker will best be
+                         * handled by unregistering old ones.
+                         */
+                        fetchAndCache(event, cache);
+                    }
+
                     return response;
-                } else {
-                    // Not cached - fetch it and then store for future network requests
-                    return fetch(event.request).then(function(fetchResponse) {
-                        var isIndexHtml = url[url.length-1] === '/' || url.split('/').pop() === 'index.html';
-
-                        if (!isIndexHtml) {
-                            /**
-                             * TODO index.html/root files should be cached in a cache-then-network fashion
-                             * Since index.html is required for the page to load (obviously), it
-                             * should be cached for offline usage. However, if index.html changes,
-                             * only returning the cached result will never show the new version to
-                             * users. Thus, this should be updated to return the cached version first
-                             * and then make a network request for the new index.html and update its
-                             * cache with the new version. This has the result of showing the old page
-                             * version first and then, on new page refresh, showing the new version.
-                             *
-                             * Note that this will only be required for index.html and other root-level
-                             * files (as declared in webpack.config.js -> CopyWebpackPlugin) because they
-                             * aren't built by webpack so they don't have a hash inserted into their filename.
-                             * Everything else will be handled correctly because if the hash is different,
-                             * then it's not in the cache, so it will be requested as normal.
-                             *
-                             * TODO clear old cache content of hashed files when new version available
-                             * So as to not take up infinite storage space on the user's device
-                             */
-                            try {
-                                cache.put(event.request, fetchResponse.clone());
-                            } catch(e) {
-                                console.log('Could not cache response. Failed with error:', e)
-                            }
-                        }
-
-                        return fetchResponse;
-                    });
+                } else { // Not cached - fetch it and then store for future network requests
+                    return fetchAndCache(event, cache);
                 }
             });
         })
