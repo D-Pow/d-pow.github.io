8198616d7c7dfa567ec053b64f1e88a321ca7ba9
D-Pow <djp460@nyu.edu>
2020-01-20 17:34:20 -0500

    Add E-Trade icon in assets and use in ETRADE_LINK
    This way, the image can be cached by the service worker and display when offline because the page origin will be the same as other images

--- src/utils/CommonRenders.js
+++ src/utils/CommonRenders.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Link from 'components/ui/Link';
+import Image from 'components/ui/Image';
 import { LINKS } from 'utils/Constants';
 
 export const ETRADE_LINK = (
     <Link href={LINKS.EtradeHome}>
-        <img
-            alt={'e-trade-icon'}
-            src={LINKS.EtradeIcon}
-            style={{ marginTop: '-1%' }}
-        />
+        <Image image={'etrade_icon.png'} aria={{ style: { marginTop: '-1%' }}} />
         E-Trade
     </Link>
 );
