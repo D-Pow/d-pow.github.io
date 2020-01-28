694ba10754a06eb9ffa16c9b36944597aecd4751
D-Pow <djp460@nyu.edu>
2020-01-27 21:51:34 -0500

    Use FlipCard in Footer
    Now that the full-flip animation is extracted to a common-use component, use it instead of manual animation handling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,34 +1,35 @@
 import React, { useState } from 'react';
+import FlipCard from 'components/ui/FlipCard';
 import { EasterEgg, SRC_CODE } from 'utils/CommonRenders';
 import 'styles/Footer.scss';
 
 function Footer(props) {
     const [ showSrcCode, setShowSrcCode ] = useState(false);
-    const animationCls = 'duration-5';
-    const showCls = 'flip-x-full';
-    const hideCls = 'd-none';
 
     const renderedPrompt = (
-        <div className={`${animationCls} ${showSrcCode ? hideCls : showCls}`}>
+        <React.Fragment>
             <div className={'footer-title'}>There are 3 easter eggs on this site.</div>
             <div className={'footer-title'}>Can you find them all?
                 <EasterEgg onClick={() => setShowSrcCode(!showSrcCode)} />
             </div>
-        </div>
+        </React.Fragment>
     );
     const renderedSrcCode = (
-        <div className={`${animationCls} ${showSrcCode ? showCls : hideCls}`}>
+        <React.Fragment>
             <div className={'footer-title'}>You just discovered the {SRC_CODE}!</div>
             <div className={'footer-title'}>
                 <EasterEgg onClick={() => setShowSrcCode(!showSrcCode)} />
             </div>
-        </div>
+        </React.Fragment>
     );
 
     return (
         <footer className={'bg-light py-3 w-100'}>
-            {renderedPrompt}
-            {renderedSrcCode}
+            <FlipCard
+                isFlipped={showSrcCode}
+                showDefault={renderedPrompt}
+                showOnClick={renderedSrcCode}
+            />
         </footer>
     );
 }
