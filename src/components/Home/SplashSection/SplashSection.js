a03a1660377decf257b2c831452ff1a3b22380c7
D-Pow <djp460@nyu.edu>
2020-05-01 17:59:09 -0400

    Merge MountainBackground and DrawingText splash sections into one, showing the mountain bg after text draws
    The drawing-text animation has a nice, clean animation mixing with the fading-out global spinner. On the other hand, the MountainBackground has a more elegant view than just div.bg-dark. Thus, mix the two together by putting the DrawingText content on top of the MountainBackground image. However, wait until after the text finishes drawing before fading the mountain image into view

--- src/components/Home/SplashSection/SplashSection.js
+++ src/components/Home/SplashSection/SplashSection.js
@@ -1,14 +1,22 @@
-import React, { useContext } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect, useContext } from 'react';
 import Image from 'components/ui/Image';
 import SvgDrawingText from 'components/ui/SvgDrawingText';
-import { isMobileBrowser } from 'utils/Functions';
 import AppContext, { AppContextFields } from 'utils/AppContext';
 import 'styles/SplashSection.scss';
 
-function DrawingTextSplashSection() {
+function SplashSection() {
     const { contextState } = useContext(AppContext.Context);
     const spinnerWasClosed = contextState[AppContextFields.GLOBAL_SPINNER_CLOSED];
+    const [ showBgImage, setShowBgImage ] = useState(false);
+    const drawingAnimationTime = 3000;
+
+    useEffect(() => {
+        if (spinnerWasClosed && !showBgImage) {
+            setTimeout(() => {
+                setShowBgImage(true);
+            }, drawingAnimationTime);
+        }
+    }, [ spinnerWasClosed ]);
 
     function renderEvenlySpacedSkewedDrawingTexts(textArray) {
         const centerX = '50%';
@@ -42,84 +50,19 @@ function DrawingTextSplashSection() {
         : '';
 
     return (
-        <div className={'full-screen-minus-scrollbar flex-center bg-dark'}>
-            <svg className={'h-75 w-80'} viewBox={'0 0 100 100'}>
-                {renderedDrawingTexts}
-            </svg>
-        </div>
-    );
-}
-
-function MountainBackgroundSplashSection() {
-    const pageText = {
-        welcomeTitle: "Hey there, I'm Devon!"
-    };
-    const textSize = isMobileBrowser() ? 'display-5' : 'display-4';
-    const DisabledTextBoldOnMobile = isMobileBrowser() ? React.Fragment : 'strong';
-    const renderedTextOnShadow = (
-        <div className={'container'}>
-            <div className={'row justify-content-center'}>
-                <div className={'col-sm-10'}>
-                    <div className={'bg-dark-fadeout rounded w-80 m-auto py-2 px-3'}>
-                        <h1 className={`text-white ${textSize}`}>
-                            <DisabledTextBoldOnMobile>
-                                {pageText.welcomeTitle}
-                            </DisabledTextBoldOnMobile>
-                        </h1>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-    const absoluteSpacedContainerClasses = 'text-center position-absolute w-100';
-    const renderedAbsoluteSpacedContainer = (
-        <React.Fragment>
-            <div className={'d-block d-sm-none'}>
-                <div className={`${absoluteSpacedContainerClasses} top-15`}>
-                    {renderedTextOnShadow}
-                </div>
-            </div>
-            <div className={'d-none d-sm-block'}>
-                <div className={`${absoluteSpacedContainerClasses} top-20`}>
-                    {renderedTextOnShadow}
-                </div>
-            </div>
-        </React.Fragment>
-    );
-
-    return (
-        <div className={'overflow-hidden'}>
+        <div className={'bg-dark full-screen-minus-scrollbar'}>
             <Image
-                className={'full-screen'}
+                className={`w-100 h-100 animated fade duration-8 ${showBgImage ? 'show' : ''}`}
                 image={'blue_horizon.svg'}
                 fluidImage={false}
             />
-            {renderedAbsoluteSpacedContainer}
+            <div className={'absolute-center'}>
+                <svg className={'h-75 w-80'} viewBox={'0 0 100 100'}>
+                    {renderedDrawingTexts}
+                </svg>
+            </div>
         </div>
     );
 }
-
-function SplashSection(props) {
-    const componentTypes = {
-        [SplashSection.Types.DRAWING_TEXT]: DrawingTextSplashSection,
-        [SplashSection.Types.MOUNTAIN_BACKGROUND]: MountainBackgroundSplashSection
-    };
-    const Component = componentTypes[props.type];
-
-    return <Component />;
-}
-
-SplashSection.Types = {
-    DRAWING_TEXT: 'DrawingText',
-    MOUNTAIN_BACKGROUND: 'MountainBackground'
-};
-
-SplashSection.propTypes = {
-    type: PropTypes.oneOf(Object.values(SplashSection.Types))
-};
-
-SplashSection.defaultProps = {
-    type: SplashSection.Types.DRAWING_TEXT
-};
 
 export default SplashSection;
