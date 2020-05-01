0bed10e49e38812c681ca5f938bf0c37c1a3aeb6
D-Pow <djp460@nyu.edu>
2020-05-01 13:33:46 -0400

    Fix automatic CSS url re-writing by webpack
    Previously, 'url(../relative/path/file)' would be re-written to e.g. 'url(static/relative/path/file)' which breaks any relative URLs. Fix that by specifying 'url: false' in css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,12 @@ module.exports = {
                             hmr: hotReloading,
                         }
                     },
-                    'css-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false
+                        }
+                    },
                     {
                         loader: 'postcss-loader',
                         options: {
