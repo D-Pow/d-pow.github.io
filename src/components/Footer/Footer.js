424a1683de010064dc4dad1a23af8f92658b3a9f
D-Pow <djp460@nyu.edu>
2020-05-03 16:21:58 -0400

    Minor: Replace manual .footer-title styling with CSS classes already present

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,23 +1,22 @@
 import React, { useState } from 'react';
 import FlipCard from 'components/ui/FlipCard';
 import { EasterEgg, SRC_CODE } from 'utils/CommonRenders';
-import 'styles/Footer.scss';
 
 function Footer(props) {
     const [ showSrcCode, setShowSrcCode ] = useState(false);
 
     const renderedPrompt = (
         <React.Fragment>
-            <div className={'footer-title'}>There are 3 easter eggs on this site.</div>
-            <div className={'footer-title'}>Can you find them all?
+            <div className={'d-block h5 text-dark'}>There are 3 easter eggs on this site.</div>
+            <div className={'d-block h5 text-dark'}>Can you find them all?
                 <EasterEgg onClick={() => setShowSrcCode(!showSrcCode)} />
             </div>
         </React.Fragment>
     );
     const renderedSrcCode = (
         <React.Fragment>
-            <div className={'footer-title'}>You just discovered the {SRC_CODE}!</div>
-            <div className={'footer-title'}>
+            <div className={'d-block h5 text-dark'}>You just discovered the {SRC_CODE}!</div>
+            <div className={'d-block h5 text-dark'}>
                 <EasterEgg onClick={() => setShowSrcCode(!showSrcCode)} />
             </div>
         </React.Fragment>
