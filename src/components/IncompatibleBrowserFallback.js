20bda967886cdfe02ce1384d985f7fa85997da29
D-Pow <djp460@nyu.edu>
2020-05-03 13:39:46 -0400

    Make 'modern browser' inclusive instead of exclusive
    Since the site works on other modern browsers, show Chrome/Firefox/Safari as examples instead of as the only available browsers

--- src/components/IncompatibleBrowserFallback.js
+++ src/components/IncompatibleBrowserFallback.js
@@ -4,7 +4,7 @@ function IncompatibleBrowserFallback(props) {
     return (
         <div className={'text-center position-absolute top-20 w-100'}>
             <h1 className={'w-80 m-auto'}>
-                Please use a modern browser (Chrome, Firefox, Safari) to view this website.
+                Please use a modern browser (e.g. Chrome, Firefox, Safari) to view this website.
             </h1>
         </div>
     );
