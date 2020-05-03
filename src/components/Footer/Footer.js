638c3ddc21ae465f69c633d2f145a4c170cbfa66
D-Pow <djp460@nyu.edu>
2020-05-03 16:27:44 -0400

    Extract repeated render code to function

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,19 +4,13 @@ import { EasterEgg, SRC_CODE } from 'utils/CommonRenders';
 
 function Footer(props) {
     const [ showSrcCode, setShowSrcCode ] = useState(false);
+    const defaultText = [ 'There are 3 easter eggs on this site.', 'Can you find them all?' ];
+    const easterEggText = [ <React.Fragment>You just discovered the {SRC_CODE}!</React.Fragment>, '' ];
 
-    const renderedPrompt = (
+    const renderTitleContent = ([ textLine1, textLine2 ]) => (
         <React.Fragment>
-            <div className={'d-block h5 text-dark'}>There are 3 easter eggs on this site.</div>
-            <div className={'d-block h5 text-dark'}>Can you find them all?
-                <EasterEgg onClick={() => setShowSrcCode(!showSrcCode)} />
-            </div>
-        </React.Fragment>
-    );
-    const renderedSrcCode = (
-        <React.Fragment>
-            <div className={'d-block h5 text-dark'}>You just discovered the {SRC_CODE}!</div>
-            <div className={'d-block h5 text-dark'}>
+            <div className={'d-block h5 text-dark'}>{textLine1}</div>
+            <div className={'d-block h5 text-dark'}>{textLine2}
                 <EasterEgg onClick={() => setShowSrcCode(!showSrcCode)} />
             </div>
         </React.Fragment>
@@ -26,8 +20,8 @@ function Footer(props) {
         <footer className={'bg-light py-3 w-100'}>
             <FlipCard
                 isFlipped={showSrcCode}
-                showDefault={renderedPrompt}
-                showOnClick={renderedSrcCode}
+                showDefault={renderTitleContent(defaultText)}
+                showOnClick={renderTitleContent(easterEggText)}
             />
         </footer>
     );
