535ed83f1230c7ec0108512bd9ec896e1445d450
D-Pow <djp460@nyu.edu>
2020-08-27 15:46:44 -0400

    Improve performance by not re-defining render function on every render
    This commit includes (1) rename function to match standard React naming conventions, and (2) move the renderText() logic outside HoverTranslate so that it isn't redefined on every render. (2) could be done with React.memo but since it's a render function, it makes logical sense to convert it to its own component

--- src/components/ui/HoverTranslate/HoverTranslate.js
+++ src/components/ui/HoverTranslate/HoverTranslate.js
@@ -2,6 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useHover } from 'utils/Hooks';
 
+function HoverText({ className, title, description, show, passedRef }) {
+    return (
+        <div className={`${className} ${show ? 'show' : ''}`} ref={passedRef}>
+            <h1 className={'font-size-2-5em'}>{title}</h1>
+            <h4 className={'font-size-1-5em'}>{description}</h4>
+        </div>
+    );
+}
+
 function HoverTranslate({
     className,
     animationCls,
@@ -18,22 +27,26 @@ function HoverTranslate({
     // TODO convert to CSS instead of JS scroll handler
     const [ ref, isHovered ] = useHover(boundingClientRectForHover);
 
-    const renderText = ({ title, description }, show, ref) => (
-        <div className={`${animationCls} ${show ? 'show' : ''}`} ref={ref}>
-            <h1 className={'font-size-2-5em'}>{title}</h1>
-            <h4 className={'font-size-1-5em'}>{description}</h4>
-        </div>
-    );
-
     // Japanese font is larger than English, so attach ref to Japanese render
     // so that the hover boundary is larger
     return (
         <div className={className} {...aria}>
             <div className={'absolute-center'}>
-                {renderText(english, isHovered, passedRef)}
+                <HoverText
+                    className={animationCls}
+                    title={english.title}
+                    description={english.description}
+                    show={isHovered}
+                    passedRef={passedRef}
+                />
             </div>
             <div className={'absolute-center'} ref={ref}>
-                {renderText(japanese, !isHovered)}
+                <HoverText
+                    className={animationCls}
+                    title={japanese.title}
+                    description={japanese.description}
+                    show={!isHovered}
+                />
             </div>
         </div>
     );
