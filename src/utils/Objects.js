7789f6d6bb6d4de2ec26290708bbc6dcb5d7e6a5
D-Pow <djp460@nyu.edu>
2020-08-27 18:17:23 -0400

    Add new util functions to determine differences in objects

--- src/utils/Objects.js
+++ src/utils/Objects.js
@@ -40,3 +40,125 @@ export function attemptParseObjLiteral(obj) {
         return obj;
     }
 }
+
+/**
+ * Determines the differences between two objects, returning all nested
+ * paths of differences.
+ *
+ * Ignores prototypes and inheritance.
+ *
+ * If arguments are not objects or arrays, then '.' will be returned if they differ.
+ *
+ * @param {Object} obj1 - 1 of 2 objects to be compared
+ * @param {Object} obj2 - 2 of 2 objects to be compared
+ * @param {boolean} [showArrayIndex=true] - If the index of arrays should be included in diff set
+ * @returns {Set<string>} - Set showing paths to nested differences
+ */
+export function diffObjects(obj1, obj2, showArrayIndex = true) {
+    // object literals, class instances, arrays, and functions
+    const areBothRealObjects = (a, b) => ((a instanceof Object) && (b instanceof Object));
+    const functionType = typeof (diffObjects);
+    const areBothFunctions = (a, b) => ((typeof a === functionType) && (typeof b === functionType));
+    const areBothArrays = (a, b) => (Array.isArray(a) && Array.isArray(b));
+
+    const differences = [];
+
+    function handleDifferentTypes(a, b, key) {
+        if (typeof a !== typeof b) {
+            differences.push(key);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    function handleNonObjects(a, b, key) {
+        if (!areBothRealObjects(a, b)) {
+            // anything not a "real" object:
+            // strings, numbers, booleans, null, undefined, and symbols
+            if (a !== b) {
+                differences.push(key);
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    function handleFunctions(a, b, key) {
+        if (areBothFunctions(a, b)) {
+            if (a.toString() !== b.toString()) {
+                differences.push(key);
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    function handleArrays(a, b, key) {
+        if (areBothArrays(a, b)) {
+            for (let i = 0; (i < a.length || i < b.length); i++) {
+                const indexKey = `[${i}]`;
+                let nestedKey = `${key}` + (showArrayIndex ? indexKey : '');
+
+                if (key === '.') {
+                    // force top-level call to show root index if array
+                    nestedKey = indexKey;
+                }
+
+                handleAllValues(a[i], b[i], nestedKey);
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    function handleObjects(a, b, key) {
+        const parentKeyPath = key === '.'
+            ? '' // don't add '.' to top-level call for objects, and clear original '.'
+            : `${key}.`; // add '.' after previous path to show it's a key from a parent object
+        const allKeysForBothObjects = new Set(Object.keys(a).concat(Object.keys(b)));
+
+        allKeysForBothObjects.forEach(nestedKey => {
+            const value1 = a[nestedKey];
+            const value2 = b[nestedKey];
+            const nestedKeyPath = `${parentKeyPath}${nestedKey}`;
+
+            handleAllValues(value1, value2, nestedKeyPath);
+        });
+
+        return true;
+    }
+
+    function handleAllValues(a, b, key) {
+        return (
+            handleDifferentTypes(a, b, key)
+            || handleNonObjects(a, b, key)
+            || handleFunctions(a, b, key)
+            || handleArrays(a, b, key)
+            || handleObjects(a, b, key)
+        );
+    }
+
+    handleAllValues(obj1, obj2, '.'); // '.' is safety check in case non-(array|object) args are passed
+
+    return new Set(differences);
+}
+
+/**
+ * Determines if two objects are equal.
+ * Ignores prototypes and inheritance.
+ *
+ * @param {Object} obj1 - 1 of 2 objects to be compared
+ * @param {Object} obj2 - 2 of 2 objects to be compared
+ * @returns {boolean} - If the stringified version of obj1 equals that of obj2
+ */
+export function objEquals(obj1, obj2) {
+    return diffObjects(obj1, obj2).size === 0;
+}
