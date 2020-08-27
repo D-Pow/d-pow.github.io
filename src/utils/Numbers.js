2cd041576a7f1d55f2d02b68ebcabf034a1d5a39
D-Pow <djp460@nyu.edu>
2020-08-27 17:12:47 -0400

    Add new util functions for distributing numbers evenly within a range
    Includes use of new util function, lerp(), which is used to get the value that is x% between min and max

--- src/utils/Numbers.js
+++ src/utils/Numbers.js
@@ -16,3 +16,38 @@ export function randomNumber(min, max) {
 
     return (Math.random() * (max - min)) + min;
 }
+
+/**
+ * Gets the value that is {@code factor} percent
+ * between {@code start} and {@code end} via
+ * linear interpolation.
+ *
+ * @param {number} start - Min value of range.
+ * @param {number} end - Max value of range.
+ * @param {number} factor - Value between 0 and 1 inclusive.
+ * @returns {number} - Value that is {@code factor} percent between {@code start} and {@code end}.
+ */
+export function lerp(start, end, factor) {
+    return start + (end - start)*factor;
+}
+
+/**
+ * Gets a list of numbers that are distributed evenly by a single factor
+ * between the range ({@code start}, {@code end}) exclusive.
+ *
+ * @param {number} start - Min value of range.
+ * @param {number} end - Max value of range.
+ * @param {number} numValues - Number of values to get within the specified range.
+ * @returns {number[]} - Numbers distributed evenly between {@code start} and {@code end}.
+ */
+export function distributeValuesEvenlyBetween(start, end, numValues) {
+    const factorToIncrementBy = 1 / (numValues + 1);
+
+    return Array
+        .from({ length: numValues })
+        .map((nul, i) => {
+            const distanceFactorOfEntry = (i + 1) * factorToIncrementBy;
+
+            return lerp(start, end, distanceFactorOfEntry);
+        });
+}
