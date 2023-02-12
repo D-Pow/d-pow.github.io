c12332defd36189c102a3adb150cbb55ea375b12
D-Pow <D-Pow@users.noreply.github.com>
2023-02-11 21:46:51 -0700

    Update root-level index.js to be JSX with modern `React.createRoot().render()` instead of `ReactDom.render()`
    Introduced in React v18

---
+++ src/index.jsx
@@ -0,0 +1,27 @@
+import { createRoot } from 'react-dom/client';
+
+import App from '@/components/App';
+import AppContext from '@/utils/AppContext';
+import registerServiceWorker from '@/registerServiceWorker';
+import '@/styles/index.scss';
+
+const { Provider } = AppContext;
+const renderedApp = (
+    <Provider>
+        <App />
+    </Provider>
+);
+const rootDiv = document.getElementById('root');
+const reactRoot = createRoot(rootDiv);
+
+reactRoot.render(renderedApp);
+
+registerServiceWorker();
+
+// hot reloading
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+    console.log('hot reloading active');
+    module.hot.accept('@/components/App', () => {
+        reactRoot.render(renderedApp);
+    })
+}
