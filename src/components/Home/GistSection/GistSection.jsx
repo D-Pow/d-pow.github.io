ea224835e9261e1f874a11af93c015239178b9cc
D-Pow <D-Pow@users.noreply.github.com>
2023-02-12 10:07:00 -0700

    Add gist for my work at Nextdoor
    Note: `<details/>` can't be a child of `<p/>` so change the parents to React.Fragment components

--- src/components/Home/GistSection/GistSection.jsx
+++ src/components/Home/GistSection/GistSection.jsx
@@ -5,20 +5,32 @@ import ScrollToShow from '@/components/ui/ScrollToShow';
 import Shape from '@/components/ui/Shape';
 import Link from '@/components/ui/Link';
 import { LINKS } from '@/utils/Constants';
-import { ETRADE_LINK } from '@/utils/CommonRenders';
+import { ETRADE_LINK, NEXTDOOR_LOGO } from '@/utils/CommonRenders';
 import { getGridBreakpoints } from '@/utils/Scss';
 
 function GistSection(props) {
     const pageText = {
         title: 'Full-stack software engineer',
         description: (
-            <React.Fragment>
-                I build platforms at {ETRADE_LINK} for creating <Link href={LINKS.MutualFundsHome}>Mutual Fund/ETF </Link>
-                investment plans. I have a passion for good coding practices, both for humans (readability, design patterns) and
-                computers (efficiency). Enjoying my work is all about learning new things, making meaningful
-                contributions, and collaborating with my teammates, because it's the people who make the workplace
-                worthwhile.
-            </React.Fragment>
+            <>
+                <div className={'mb-5p'}>
+                    I'm a lead front-end application architect at {NEXTDOOR_LOGO}, where I focus on improving devs' lives,
+                    including everything from accelerating build speed and client performance, to code architecture and
+                    modernization, and even to tool optimization.
+                    Enhancing our devs' lives means features get delivered faster, stabler, and cleaner, resulting in an
+                    overall more enjoyable experience for us as well as all our users.
+                </div>
+
+                <details>
+                    <summary>I've also worked at E-Trade!</summary>
+
+                    I built platforms at {ETRADE_LINK} for creating <Link href={LINKS.MutualFundsHome}>Mutual Fund/ETF</Link>{' '}
+                    investment plans. I have a passion for good coding practices, both for humans (readability, design patterns) and
+                    computers (efficiency). Enjoying my work is all about learning new things, making meaningful
+                    contributions, and collaborating with my teammates, because it's the people who make the workplace
+                    worthwhile.
+                </details>
+            </>
         ),
     };
     const mainContentShowThreshold = 2/3;
@@ -39,7 +51,7 @@ function GistSection(props) {
                         <div className={'col-sm-6 m-auto pb-4 slide-in-left'}>
                             <div className={'m-auto'}>
                                 <h3 className={'p-2'}>{pageText.title}</h3>
-                                <p>{pageText.description}</p>
+                                <>{pageText.description}</>
                             </div>
                         </div>
                         <div className={'col-sm-6 m-auto slide-in-right'}>
