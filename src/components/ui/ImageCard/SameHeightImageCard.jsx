cb7b9c584da44d1e83d98cfc2256045ec47b37ba
D-Pow <D-Pow@users.noreply.github.com>
2023-03-18 18:58:50 -0600

    Bug fix: Make `<SameHeightImageCard/>` grow if screen space allows larger images to be rendered
    Previously, the context state logic would only ever shrink images. The problem with that is if the images were shrunk upon first resize (e.g. if window width is decreased), then the images' new min/max height would be constricted to that shrunk size. If the window width were increased afterwards such that theoretically the image heights could also increase, they'd either stay the same or *decrease further* since the `<ImageCard/>` height was set to that previous, smaller height. This meant the images would rarely be the correct size that they could be on a given screen size, and caused them to repetitively shrink upon window width grow *and* shrink.
    
    Fix this by creating a new "ghost" `<ImageCard/>` with most of the same styling as the "real" `<ImageCard/>` except with `height: auto` to see what the height would have actually been if we had just rendered the image naturally. This way, we always get an accurate reading of what the max/min heights of the images could be and can thus make an informed, correct decision on what the final heights should be.

--- src/components/ui/ImageCard/SameHeightImageCard.jsx
+++ src/components/ui/ImageCard/SameHeightImageCard.jsx
@@ -48,12 +48,46 @@ function SameHeightImageCard({ imageAria, imageStyle, onLoad, ...imageCardProps 
     }, [ windowSizeState.wasResized, windowSizeState.prevWidth ]);
 
     return (
-        <ImageCard
-            {...imageCardProps}
-            imageStyle={{ height: `${getSmallestImageHeightFromContext()}px`, overflow: 'hidden', ...imageStyle }}
-            imageAria={{ ref: imageRef, ...imageAria }}
-            onLoad={handleOnLoad}
-        />
+        <>
+            <ImageCard
+                {...imageCardProps}
+                imageStyle={{ height: `${getSmallestImageHeightFromContext()}px`, overflow: 'hidden', ...imageStyle }}
+                imageAria={imageAria}
+                onLoad={handleOnLoad}
+            />
+            <ImageCard
+                /*
+                 * Create ghost element to get the exact, correct size the image would be without height restrictions,
+                 * so we can use that height as our basis for the same-height calculations, rather than using the
+                 * previously-shrunk height that biases subsequent same-height calculations.
+                 *
+                 * See:
+                 *  - https://stackoverflow.com/questions/60235415/how-do-i-make-images-the-same-height-in-a-flexbox-side-by-side-without-knowing
+                 *  - https://stackoverflow.com/questions/47236454/css-flexbox-organizing-flex-items-based-on-screen-size
+                 */
+                // Use same props and styles as the actually-rendered content to mimic the same behaviors.
+                {...imageCardProps}
+                // Set `position: absolute` on this ghost since `visibility: hidden` makes the element take up space
+                // in the display layout (i.e. they take up space).
+                // Note: Centering it is fine since this component is only ever used in a `.col` parent wrapper,
+                // however we might need to revisit this in the future if that fact changes.
+                wrapperCls={`${imageCardProps.wrapperCls} absolute-center`}
+                imageStyle={{
+                    // Use `height: auto` to obtain the largest possible height the image could be.
+                    height: 'auto',
+                    overflow: 'hidden',
+                    ...imageStyle,
+                    // Hide this element from display, but allow it to take up space since we need it to take up
+                    // space in order to get the height it would be normally.
+                    visibility: 'hidden',
+                }}
+                // Attach the ref here so this ghost image's height is what's used in calculations.
+                // Otherwise, the shown image will just continue to get smaller and smaller with every window-size change
+                // due to our `height: getSmallestImageHeightFromContext()` styling in the shown, "real" image.
+                imageAria={{ ref: imageRef, ...imageAria }}
+                showHoverContent={false}
+            />
+        </>
     );
 }
 
